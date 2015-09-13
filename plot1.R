27ea55f04c856e928491df4569acb74c588f3260
wizor2@yahoo.com <wizor2@yahoo.com>
2015-09-13 14:52:32 +0200

    final version

--- plot1.R
+++ plot1.R
@@ -1,9 +1,12 @@
+## read data from file to table and change class for Date variable in table
 sourceData <- read.table("./household_power_consumption.txt", sep = ";", header = TRUE)
 sourceData$Date <- as.Date(as.character(sourceData$Date), format = "%d/%m/%Y")
 
+## create new table from original according project conditions and change class for Global_active_power variable
 dateData <- subset(sourceData, Date >= "2007-02-01" & Date <= "2007-02-02")
 dateData$Global_active_power <- as.numeric(as.character(dateData$Global_active_power))
 
+## write histogram in file 'plot1.png'
 png(file = "plot1.png")
 hist(dateData$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Power Active (kilowatts)")
 dev.off()
